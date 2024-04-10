cc458ca5e585213aedc0fd16466a339d71fc5a66
daniemc <daniel.meneses27@gmail.com>
2024-04-09 21:21:02 -0500

    Project setup
    - Add github config to CI to AWS

--- docker/run.sh
+++ docker/run.sh
@@ -6,12 +6,12 @@ composer update
 
 php artisan storage:link
 
-php artisan migrate
+# php artisan migrate
 
 php artisan config:cache
 
 php artisan route:cache
 
-php artisan view:cache
+# php artisan view:cache
 
 /usr/bin/supervisord -c /etc/supervisord.conf
